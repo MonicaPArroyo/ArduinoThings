50cd2c779dc0021f64c6d5e58852ef4df9dc06be
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2022-01-20 00:54:22 -0600

    Update Servomotor.ino

--- Actuadores/Servomotor/Servomotor.ino
+++ Actuadores/Servomotor/Servomotor.ino
@@ -4,17 +4,16 @@
  */
 
 #include <Servo.h> // Incluimos la librería para poder controlar el servo
- 
 Servo servoMotor; // Declaramos la variable para controlar el servo
+
+int grados = 0; // Declaramos la variable para los grados
+int tiempo = 100; // Declaramos la variable para el tiempo
 
 void setup() {
   servoMotor.attach(9); // Iniciamos el servo para que empiece a trabajar con el pin 9
 }
  
 void loop() {
-  int grados = 0; // Declaramos la variable para los grados
-  int tiempo = 100; // Declaramos la variable para el tiempo
-  
   servoMotor.write(grados);// Desplazamos a la posición que contiene la variable grados
   delay(tiempo);// Esperamos el tiempo que contiene la variable tiempo
 }
